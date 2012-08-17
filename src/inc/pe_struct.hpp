1fec0df768a5a58026760b1b5a4ee01af0565487
Axel "0vercl0k" Souchet <0vercl0k@tuxfamily.org>
2012-08-17 11:17:55 -0400

    add the GPLv3 stuff

--- src/inc/pe_struct.hpp
+++ src/inc/pe_struct.hpp
@@ -1,3 +1,22 @@
+/*
+    This file is part of rp++.
+
+    Copyright (C) 2012, Axel "0vercl0k" Souchet <0vercl0k at tuxfamily.org>
+    All rights reserved.
+
+    rp++ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    rp++ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with rp++.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef PE_STRUCT_H
 #define PE_STRUCT_H
 
