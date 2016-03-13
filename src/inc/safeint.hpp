fcc4e28e9612a816574ced6a12b01da3e77226cc
Axel "0vercl0k" Souchet <0vercl0k@tuxfamily.org>
2016-03-13 13:51:27 -0700

    modernization: ensure only unsigned types as the logic does not apply to signed ones

--- src/inc/safeint.hpp
+++ src/inc/safeint.hpp
@@ -27,7 +27,9 @@
     The purpose of this class is to avoid integer overflow ; if one is detected, an exception is raised
 */
 template<class T>
-T SafeIntAdd(const T a, const T b)
+typename std::enable_if<
+	std::is_unsigned<T>::value, T
+>::type SafeIntAdd(const T a, const T b)
 {
 	if(a > (std::numeric_limits<T>::max() - b))
 		RAISE_EXCEPTION("Integer-overflow detected.");
